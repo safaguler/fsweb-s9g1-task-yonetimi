673cc8afa770ade80cba615fa3c4138b02313971
safaguler <130096111+safaguler@users.noreply.github.com>
2023-09-11 14:01:20 +0300

    görev yapılmıştır.

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ function App() {
   const [team, setTeam] = useState(initialTeam);
 
   function handleTaskSubmit(yeniTask) {
-    setTasks([yeniTask, ...tasks])
+    setTasks([yeniTask, ...tasks]);
+    console.log('Yeni görev eklendi: ', yeniTask.title);
   }
 
   function handlePeopleSubmit(yeniKisi) {
@@ -20,46 +21,49 @@ function App() {
   }
 
   function handleComplete(id) {
-    console.log("tamamlama fonksiyonunu buraya yazın")
+    const updatedTasks = tasks.map((task) =>
+      task.id === id ? { ...task, status: 'yapıldı' } : task
+    );
+    setTasks(updatedTasks);
+    console.log('Görev tamamlandı: ', id);
   }
 
   return (
     <div className="app">
-      <div className="formColumn">
-        <div className="form-container">
-          <h2>Yeni Task</h2>
-          {/* <TaskForm kisiler={team} submitFn={handleTaskSubmit} /> */}
-          <TaskHookForm kisiler={team} submitFn={handleTaskSubmit} />
-        </div>
+       <div className="formColumn">
+  <div className="form-container">
+    <h2>Yeni Task</h2>
+    <TaskForm kisiler={team} submitFn={handleTaskSubmit} />
+   <TaskHookForm kisiler={team} submitFn={handleTaskSubmit} /> 
+  </div>
 
-        <div className="form-container">
-          <h2>Yeni Kişi</h2>
-          <PeopleForm kisiler={team} submitFn={handlePeopleSubmit} />
-        </div>
-      </div>
-      <div className="columns">
-        <div className="column">
-          <h2 className="column-title">Yapılacaklar</h2>
-          <div className="column-list">
-            {tasks
-              .filter((t) => t.status === "yapılacak")
-              .map((t) => (
-                <Task key={t.id} taskObj={t} onComplete={handleComplete} />
-              ))}
-          </div>
-        </div>
-        <div className="column">
-          <h2 className="column-title">Tamamlananlar</h2>
-          <div className="column-list">
-            {tasks
-              .filter((t) => t.status === "yapıldı")
-              .map((t) => (
-                <Task key={t.id} taskObj={t} />
-              ))}
-          </div>
-        </div>
-      </div>
-
+  <div className="form-container">
+    <h2>Yeni Kişi</h2>
+    <PeopleForm kisiler={team} submitFn={handlePeopleSubmit} />
+  </div>
+</div>
+<div className="columns">
+  <div className="column">
+    <h2 className="column-title">Yapılacaklar</h2>
+    <div className="column-list">
+      {tasks
+        .filter((t) => t.status === "yapılacak")
+        .map((t) => (
+          <Task key={t.id} taskObj={t} onComplete={handleComplete} />
+        ))}
+    </div>
+  </div>
+  <div className="column">
+    <h2 className="column-title">Tamamlananlar</h2>
+    <div className="column-list">
+      {tasks
+        .filter((t) => t.status === "yapıldı")
+        .map((t) => (
+          <Task key={t.id} taskObj={t} />
+        ))}
+    </div>
+  </div>
+</div>
     </div>
   );
 }
--- src/TaskHookForm.js
+++ src/TaskHookForm.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
-export default function TaskHookForm() {
+export default function TaskHookForm({ kisiler, submitFn }) {
+  const { register, handleSubmit, reset } = useForm();
+
+  const onSubmit = (data) => {
+    submitFn({
+      id: Date.now(),
+      title: data.title,
+      assignee: data.assignee,
+      status: 'yapılacak',
+    });
+    reset();
+  };
+
   return (
-    <div>Formunuzu react-hook-form kullanarak burada oluşturun. TaskForm dosyasındaki HTML yapısını vs app.css içerisindeki classları kullanabilirsiniz.</div>
-  )
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-group">
+          <label>Görev Başlığı</label>
+          <input {...register('title', { required: true })} />
+        </div>
+        <div className="form-group">
+          <label>Atanan Kişi</label>
+          <select {...register('assignee', { required: true })}>
+            {kisiler.map((kisi) => (
+              <option key={kisi.id} value={kisi.id}>
+                {kisi.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <button type="submit">Görev Ekle</button>
+      </form>
+    </div>
+  );
 }
